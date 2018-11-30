6e23bced7739d96ff61a2101e5ef8022bd4ce376
Oliver Dick <o.dick@hosting.de>
2018-11-30 11:38:34 +0100

    Find Methods for Bundles & Accounts

--- src/BundleApi.php
+++ src/BundleApi.php
@@ -4,4 +4,21 @@ namespace Hostingde\API;
 
 class BundleApi extends GenericApi {
 	protected $location = 'https://secure.hosting.de/api/bundle/v1/json';
+
+	public function bundlesFind($filter, $limit = 50, $page = 1, $sort = NULL) {
+		$data = array('authToken' => $this->authToken, 'filter' => $filter, 'limit' => $limit, 'page' => $page, 'sort' => $sort);
+
+		$this->send('bundlesFind', $data);
+		if ($this->getStatus() == "error") {
+			return false;
+		}
+		if ($this->getValue()->totalEntries > 0) {
+			$return = array();
+			foreach($this->getValue()->data as $bundle) {
+				$return[] = new Bundle($bundle);
+			}
+			return $return;
+		}
+		return array();
+	}
 }
