46c1be6f299932b2ce59fe8cd0218aece3adf05d
Oliver Dick <o.dick@hosting.de>
2019-02-22 16:08:54 +0100

    databaseUpdate fuer DatabaseAPI

--- src/DatabaseApi.php
+++ src/DatabaseApi.php
@@ -35,6 +35,19 @@ class DatabaseApi extends GenericApi {
 		return new Database($this->getValue());
 	}
 
+	public function databaseUpdate($database, $accesses) {
+		$data = array("authToken" => $this->authToken, "database" => $database, "accesses" => $accesses);
+		if (isset($database->accountId)) {
+			$data['ownerAccountId'] = $database->accountId;
+		}
+
+		$this->send('databaseUpdate', $data);
+		if ($this->getStatus() == "error") {
+			return false;
+		}
+		return new Database($this->getValue());
+	}
+
 	public function usersFind($filter, $limit = 50, $page = 1, $sort = NULL) {
 		$data = array('authToken' => $this->authToken, 'filter' => $filter, 'limit' => $limit, 'page' => $page, 'sort' => $sort);
 
