01632196152e1a1a0a2a4028db937202bd0c0c62
Oliver Dick <o.dick@hosting.de>
2019-03-06 17:20:16 +0100

    Filter ohne Subfilter moeglich

--- src/classes/Filter.php
+++ src/classes/Filter.php
@@ -6,7 +6,20 @@ class Filter extends GenericObject {
 	public $subFilterConnective = 'AND';
 	public $subFilter = array();
 
+	public function __construct($subfilter = true) {
+		if (!$subfilter) {
+			unset($this->subFilterConnective);
+			unset($this->subFilter);
+		}
+	}
+
 	public function addFilter($field, $value, $relation = "equal") {
-		$this->subFilter[] = array('field' => $field, 'value' => $value, 'relation' => $relation);
+		if (isset($this->subFilter) && is_array($this->subFilter)) {
+			$this->subFilter[] = array('field' => $field, 'value' => $value, 'relation' => $relation);
+		} else {
+			$this->field = $field;
+			$this->value = $value;
+			$this->relation = $relation;
+		}
 	}
 }
