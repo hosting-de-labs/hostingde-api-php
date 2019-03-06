da2fac89d685a8dd316e595753f18caf74f1a006
Oliver Dick <o.dick@hosting.de>
2019-03-06 17:21:10 +0100

    Filter ohne Subfilter moeglich

--- src/classes/Filter.php
+++ src/classes/Filter.php
@@ -6,7 +6,8 @@ class Filter extends GenericObject {
 	public $subFilterConnective = 'AND';
 	public $subFilter = array();
 
-	public function __construct($subfilter = true) {
+	public function __construct($object = NULL, $subfilter = true) {
+		parent::__construct($object);
 		if (!$subfilter) {
 			unset($this->subFilterConnective);
 			unset($this->subFilter);
