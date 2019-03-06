691c1493de64a0e76c8501a354641fecb1809c10
Oliver Dick <o.dick@hosting.de>
2019-03-06 17:28:46 +0100

    ArticlePurchasePricesFind

---
+++ src/classes/billing/ArticlePurchasePrice.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Hostingde\API;
+
+class ArticlePurchasePrice extends GenericObject {
+	public $addGracePeriod;
+	public $addGracePeriodQuantity;
+	public $contractPeriod;
+	public $currency;
+	public $exchangeRatio;
+	public $name;
+	public $noticePeriod;
+	public $platformId;
+	public $price;
+	public $productCode;
+	public $promotionEndPIT;
+	public $promotionStartPIT;
+	public $promotionalPrice;
+	public $service;
+	public $startPIT;
+	public $vatRate;
+}
