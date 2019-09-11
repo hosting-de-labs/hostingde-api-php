d53ffaa863240ff829f8648d93c487cafd91ca5b
Oliver Dick <o.dick@hosting.de>
2019-09-11 10:16:13 +0200

    Domainpreise abrufen

---
+++ src/classes/billing/DomainPrice.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Hostingde\API;
+
+class DomainPrice extends GenericObject {
+	public $create;
+	public $createDuration;
+	public $currency;
+	public $domainSuffix;
+	public $exchangeRatio;
+	public $ownerChange;
+	public $periodOfNotice;
+	public $renew;
+	public $renewDuration;
+	public $restore;
+	public $transfer;
+	public $transferDuration;
+	public $trusteeService;
+	public $update;
+	public $vatRate;
+}
